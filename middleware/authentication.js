aa9af4bcee9693274a9375d8fab26a3adfc0cb2f
Prafull Epili <prafullepili@gmail.com>
2023-07-19 11:14:28 +0530

    restrict test user to CURD

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,7 +13,8 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name }
+    const testUser = payload.userId === '64b52b69a33fe8428488ddd3';
+    req.user = { userId: payload.userId, name: payload.name, testUser }
     next()
   } catch (error) {
     throw new UnauthenticatedError('Authentication invalid')
